30fd5b3cc464c15043bad68367e5fd9cacda9516
Anthony Suarez <asuarezop@gmail.com>
2023-08-27 20:59:15 -0400

    Added styling to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,34 @@
 import "./Header.scss";
+import mainLogo from "../../assets/logo/VisualSonic Logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <nav className="nav">
+          <div className="nav-logo">
+            <img
+              className="nav-logo__main"
+              src={mainLogo}
+              alt="VisualSonic logo"
+            />
+          </div>
+          <div className="nav-links">
+            <Link to="/login">
+              <p className="nav-links__link">Login</p>
+            </Link>
+            <Link to="/signup">
+              <p className="nav-links__link">Sign Up</p>
+            </Link>
+            <Link to="/about">
+              <p className="nav-links__link">About</p>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
