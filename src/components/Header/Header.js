4c98c8b5dbdafe56be90333282079534b464c04c
Anthony Suarez <asuarezop@gmail.com>
2023-08-27 21:40:36 -0400

    Adjusted styling on header for mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,28 +5,30 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="header">
-        <nav className="nav">
-          <div className="nav-logo">
-            <img
-              className="nav-logo__main"
-              src={mainLogo}
-              alt="VisualSonic logo"
-            />
-          </div>
-          <div className="nav-links">
-            <Link to="/login">
-              <p className="nav-links__link">Login</p>
-            </Link>
-            <Link to="/signup">
-              <p className="nav-links__link">Sign Up</p>
-            </Link>
-            <Link to="/about">
-              <p className="nav-links__link">About</p>
-            </Link>
-          </div>
-        </nav>
-      </header>
+      <main>
+        <header className="header">
+          <nav className="nav">
+            <div className="nav-logo">
+              <img
+                className="nav-logo__main"
+                src={mainLogo}
+                alt="VisualSonic logo"
+              />
+            </div>
+            <div className="nav-links">
+              <Link to="/login">
+                <p className="nav-links__link">Login</p>
+              </Link>
+              <Link to="/signup">
+                <p className="nav-links__link">Sign Up</p>
+              </Link>
+              <Link to="/about">
+                <p className="nav-links__link nav-links__link--hidden">About</p>
+              </Link>
+            </div>
+          </nav>
+        </header>
+      </main>
     </>
   );
 }
