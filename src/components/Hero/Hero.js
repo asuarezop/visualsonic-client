ce0ec4f66154be8b40b6ebff52cfb340b91da0da
Anthony Suarez <asuarezop@gmail.com>
2023-08-28 01:16:35 -0400

    Adjusted styling for Hero component on mobile and tablet

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,9 @@ function Hero() {
             Designed for the busy creator and audio enthusiast. We provide the
             tools to augment your music and your artist brand.
           </p>
-          <button className="hero-text__trynow">Try for free</button>
+          <button className="hero-text__trynow hero-text__trynow--cta">
+            Try for free
+          </button>
         </div>
       </section>
     </>
