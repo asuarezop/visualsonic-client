c6c0ed331c370d4a5f47b159635ab0dfa060434d
Anthony Suarez <asuarezop@gmail.com>
2023-08-29 21:57:50 -0400

    Added new Steps component with basic HTML structure

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -1,7 +1,14 @@
 import "./Steps.scss";
 
 function Steps() {
-  return <></>;
+  return (
+    <>
+      <section className="steps">
+        <h2 className="steps__title">Getting Started</h2>
+        <div className="steps-col"></div>
+      </section>
+    </>
+  );
 }
 
 export default Steps;
