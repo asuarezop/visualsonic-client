6d9ca74f055cb6696b8aafea78d5f53eb5f4364f
Anthony Suarez <asuarezop@gmail.com>
2023-08-31 14:07:10 -0400

    Adjusted html elements inside of SignUpPage for desktop

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -6,81 +6,82 @@ function SignUpPage() {
   }
   return (
     <>
-      <form className="sign-up-form" onSubmit={handleSubmit}>
-        <h1 className="sign-up-form__title">
+      <section className="sign-up">
+        <h1 className="sign-up__title">
           Register to save your own visualizer profile
         </h1>
+
         <div className="sign-up-card">
-          <div className="form-input">
-            <label className="form-input__title">
-              First Name:
-              <input
-                className="form-input__firstname"
-                id="firstName"
-                type="text"
-                name="firstName"
-                placeholder="First name"
-              ></input>
-            </label>
-          </div>
-          <div className="form-input">
-            <label className="form-input__title">
-              Last Name:
-              <input
-                className="form-input__lastname"
-                id="lastName"
-                type="text"
-                name="lastName"
-                placeholder="Last name"
-              ></input>
-            </label>
-          </div>
-          <div className="form-input">
-            <label className="form-input__title">
-              Email:
-              <input
-                className="form-input__email"
-                id="email"
-                type="text"
-                name="email"
-                placeholder="joe@gmail.com"
-              ></input>
-            </label>
-          </div>
-          <div className="form-input">
-            <label className="form-input__title">
-              Password:
-              <input
-                className="form-input__password"
-                id="password"
-                type="text"
-                name="password"
-                placeholder="Password must have at least 8 characters"
-              ></input>
-            </label>
-          </div>
-          <section className="profile">
-            <h2 className="profile__title">Visualizer Profile</h2>
-            <div className="customize-options">
-              <h3 className="customize-options__color">
-                Choose your own color for visualizer
-              </h3>
-              <input
-                type="color"
-                id="userColor"
-                name="userColor"
-                value="#007ea7"
-              />
-              <label htmlFor="userColor">Color</label>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="form-input">
+              <label className="form-input__title">
+                First Name:
+                <input
+                  className="form-input__firstname"
+                  id="firstName"
+                  type="text"
+                  name="firstName"
+                  placeholder="First name"
+                ></input>
+              </label>
             </div>
-          </section>
-          <div className="form-button">
-            <button className="form-button__submit" type="submit">
-              Submit Form
-            </button>
-          </div>
+            <div className="form-input">
+              <label className="form-input__title">
+                Last Name:
+                <input
+                  className="form-input__lastname"
+                  id="lastName"
+                  type="text"
+                  name="lastName"
+                  placeholder="Last name"
+                ></input>
+              </label>
+            </div>
+            <div className="form-input">
+              <label className="form-input__title">
+                Email:
+                <input
+                  className="form-input__email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  placeholder="joe@gmail.com"
+                ></input>
+              </label>
+            </div>
+            <div className="form-input">
+              <label className="form-input__title">
+                Password:
+                <input
+                  className="form-input__password"
+                  id="password"
+                  type="text"
+                  name="password"
+                  placeholder="Password must have at least 8 characters"
+                ></input>
+              </label>
+            </div>
+            <div className="form-input">
+              <label className="form-input__title">
+                Visualizer Color:
+                <input
+                  className="form-input__color"
+                  id="visualizerColor"
+                  type="color"
+                  name="visualizerColor"
+                  value="#007ea7"
+                  placeholder="Choose your visualizer color"
+                ></input>
+              </label>
+            </div>
+            <div className="form-button">
+              <button className="form-button__submit" type="submit">
+                Submit Form
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </section>
     </>
   );
 }
