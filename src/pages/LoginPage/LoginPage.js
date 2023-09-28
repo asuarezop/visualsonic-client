92d527466eb8b351ab47127670bd1826f9b53906
Anthony Suarez <asuarezop@gmail.com>
2023-09-28 15:35:02 -0400

    Added styling to login page for mobile, tablet, and desktop

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import './LoginPage.scss';
+import visualizerGif from '../../assets/images/ncs.gif';
 
 function LoginPage() {
   function handleSubmit(e) {
@@ -6,20 +7,56 @@ function LoginPage() {
   }
   return (
     <>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div className="form-input">
-          <label className="form-input__title">
-            Email
-            <input
-              className="form-input__email-title"
-              id="title"
-              type="text"
-              name="email"
-              placeholder="Email"
-            ></input>
-          </label>
+      <section className="login">
+        <div className="login-card">
+          <h1 className="login-card__title">Sign In</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="form-input">
+              <label className="form-input__title">
+                Email:
+                <input
+                  className="form-input__email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                ></input>
+              </label>
+            </div>
+            <div className="form-input">
+              <label className="form-input__title">
+                Password:
+                <input
+                  className="form-input__password"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                ></input>
+              </label>
+            </div>
+            <div className="form-button">
+              <button
+                className="form-button__submit form-button__submit--cta"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+        <div className="sign-up-preview">
+          <h2 className="sign-up-preview__title">Welcome to VisualSonic</h2>
+          <p className="sign-up-preview__sub-heading">
+            Elevate your music experience through our pre-built audio visualizer
+          </p>
+          <img
+            className="sign-up-preview__gif"
+            src={visualizerGif}
+            alt="audio visualizer gif"
+          />
+        </div>
+      </section>
     </>
   );
 }
