2e6848ce0d293b8a2d1a310f53a671900b3ddb72
Anthony Suarez <asuarezop@gmail.com>
2025-05-07 23:48:42 -0400

    Moved signInEmailAcct function to sigInProviders.js

--- src/features/auth/signInProviders.js
+++ src/features/auth/signInProviders.js
@@ -4,12 +4,22 @@ import {
   microsoftProvider,
   googleProvider,
   facebookProvider,
-} from '../../firebase/firebase';
+} from '../../firebase/firebase.js';
 import {
   signInWithPopup,
   FacebookAuthProvider,
   linkWithRedirect,
+  getRedirectResult,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
+
+export const signInEmailAcct = async (auth, email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const signInGoogle = async () => {
   try {
@@ -21,8 +31,17 @@ export const signInGoogle = async () => {
 
 export const signInMicrosoft = async () => {
   try {
-    const result = await signInWithPopup(auth, microsoftProvider);
-    console.log(result.user);
+    await linkWithRedirect(loginAuth.currentUser, microsoftProvider).then(
+      (res) => {
+        const user = res.user;
+        console.log(user);
+
+        const redirectRes = getRedirectResult(auth);
+        console.log(redirectRes);
+      }
+    );
+    // const result = await signInWithPopup(auth, microsoftProvider);
+    // console.log(result.user);
   } catch (err) {
     console.log('Error during Microsoft sign-in:', err);
     throw err;
@@ -32,12 +51,12 @@ export const signInMicrosoft = async () => {
 export const signInFacebook = async () => {
   try {
     linkWithRedirect(loginAuth.currentUser, facebookProvider).then((res) => {
-      // const fbCredential = FacebookAuthProvider.credentialFromResult(res);
-      // const user = res.user;
-      // console.log(fbCredential, user);
+      const fbCredential = FacebookAuthProvider.credentialFromResult(res);
+      const user = res.user;
+      console.log(fbCredential, user);
+      // console.log(res.user);
     });
-    const result = await signInWithPopup(auth, facebookProvider);
-    console.log(result.user);
+    // const result = await signInWithPopup(auth, facebookProvider);
   } catch (err) {
     console.log('Error during Facebook sign-in:', err);
     throw err;
