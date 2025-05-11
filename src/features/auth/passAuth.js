752d4c6342798431decf3d7423d146120dee5acd
Anthony Suarez <asuarezop@gmail.com>
2025-05-11 17:32:41 -0400

    Included logic to handle if user types in a blank email in input field

--- src/features/auth/passAuth.js
+++ src/features/auth/passAuth.js
@@ -1,5 +1,13 @@
 import { sendPasswordResetEmail } from 'firebase/auth';
 
 export const forgotPassword = async (auth, email) => {
-  await sendPasswordResetEmail(auth, email);
+  try {
+    if (email === '') {
+      alert('Please enter your email address.');
+      return;
+    }
+    await sendPasswordResetEmail(auth, email);
+  } catch (err) {
+    console.error(err);
+  }
 };
